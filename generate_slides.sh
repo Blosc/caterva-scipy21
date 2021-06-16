141c7765d9b2e762b7bcc08c4d2a7683c6ffd94b
Aleix Alcacer <aleixalcacer@gmail.com>
2021-06-16 09:56:23 +0200

    Change html output name

--- generate_slides.sh
+++ generate_slides.sh
@@ -4,9 +4,11 @@ echo "Converting from myst to ipynb..."
 jupytext --to ipynb caterva_workshop.md
 
 echo "Generating slides from ipynb..."
-jupyter nbconvert --to slides --output-dir html *.ipynb;
+jupyter nbconvert --to slides --output-dir html caterva_workshop.ipynb;
 
 echo "Cleaning up..."
 rm *.ipynb
+
+mv html/caterva_workshop.slides.html html/index.html
 
 echo "Done!";
